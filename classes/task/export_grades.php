80fcea6bb9c61391299ead1190dc9f7164dfdfec
Sarah Cotton <sarah.cotton@solent.ac.uk>
2021-02-04 10:38:48 +0000

    Implement lock for export grades task

--- classes/task/export_grades.php
+++ classes/task/export_grades.php
@@ -13,18 +13,43 @@ class export_grades extends \core\task\scheduled_task {
         require_once($CFG->dirroot.'/local/quercus_tasks/lib.php');
         // Get records with response of null for processing
         $dataarray = get_retry_list();
-
         if(count($dataarray) > 0){
           foreach ($dataarray as $key => $value) {
-            $dataready =  json_encode($value);
-            $response = export_grades($dataready);
+		
+			// 5 seconds.
+			$timeout = 5;
+			// A namespace for the locks. Must be prefixed with the component name to prevent conflicts.
+			$locktype = 'local_quercus_tasks_exportgrades'; 
+			// Resource key - needs to uniquely identify the resource that is to be locked. E.g. If you
+			// want to prevent a user from running multiple course backups - include the userid in the key.
+			$resource = key($dataarray);
+			 
+			// Get an instance of the currently configured lock_factory.
+			$lockfactory = \core\lock\lock_config::get_lock_factory($locktype);
+			 
+			// Get a new lock for the resource, wait for it if needed.
+			if ($lock = $lockfactory->get_lock($resource, $timeout)) {
+				// We have exclusive access to the resource, do the export						 
+				try {
+					$dataready =  json_encode($value);
+					$response = export_grades($dataready);
 
-            if($response != null){
-              $moduleinstanceid = update_log($response);
-              mtrace('Released grades have been processed for ' . $moduleinstanceid);
-            }else{
-              mtrace('Can\'t connect to Quercus');
-            }
+					if($response != null){
+					  $moduleinstanceid = update_log($response);
+					  mtrace('Released grades have been processed for ' . $moduleinstanceid);
+					}else{
+					  mtrace('Can\'t connect to Quercus');
+					}
+				} catch (\Throwable $e) {
+					throw $e;
+				} finally {
+					$lock->release();
+				}
+			 
+			} else {
+				// We did not get access to the resource in time, give up.
+				throw new moodle_exception('locktimeout');
+			}
           }
         }else{
           mtrace('No grades have been released');
