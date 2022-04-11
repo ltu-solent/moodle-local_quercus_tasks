fa5aed775cdac67c222a64943dcba360a224ffec
Mark Sharp <mark.sharp@solent.ac.uk>
2022-04-11 11:47:38 +0100

    Bump version

--- version.php
+++ version.php
@@ -2,6 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021020400;   // The (date) version of this plugin
+$plugin->version  = 2021020401;   // The (date) version of this plugin
 $plugin->requires = 2015111603;   // Requires this Moodle version
+$plugin->release = 'v3.9.2';
 $plugin->component = 'local_quercus_tasks';
