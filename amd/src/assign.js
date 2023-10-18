e6c4102ea269ab02f75279f3d869e22aa2c5e778
Mark Sharp <mark.sharp@solent.ac.uk>
2023-10-18 14:14:47 +0100

    Not using amd so remove

--- amd/src/assign.js
+++
@@ -1,8 +0,0 @@
-export const modedit = () => {
-    const assigneditpage = document.querySelector('#page-mod-assign-mod #region-main form');
-    if (assigneditpage) {
-        // Disable cmidnumber so it can't be filled in.
-        assigneditpage.querySelector(`[name="cmidnumber"]`)
-            .setAttribute('disabled', 'disabled');
-    }
-};
