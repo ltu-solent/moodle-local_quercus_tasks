ab6d7dd27f19671e80197d432b77a959b0afc58d
Mark Sharp <mark.sharp@solent.ac.uk>
2025-01-28 11:35:08 +0000

    Reorder language strings

--- lang/en/local_quercus_tasks.php
+++ lang/en/local_quercus_tasks.php
@@ -25,16 +25,18 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$string['acadyear'] = 'Academic year in view to filter data by';
+$string['addnewassign'] = 'Add new assignments';
 $string['assign_filterwarning'] = '<i class="fa fa-warning"></i> <strong>Warning:</strong> You are not displaying all users and will not be able to release your grades. {$a->msg}';
 $string['assign_formativeinfo'] = '<i class="fa fa-info-circle"></i> The marks for these assignments will not be uploaded to Quercus as this is not a Quercus Assignment.';
 $string['assign_resetprefs'] = ' <a href="{$a->url}">"Reset your table preferences"</a>.';
 $string['assign_resetworkflow'] = ' <a href="{$a->url}">Set all Options to "No filter"</a>.';
-$string['pluginname'] = 'Quercus Tasks';
-$string['acadyear'] = 'Academic year in view to filter data by';
-$string['addnewassign'] = 'Add new assignments';
 $string['assignmentsettings'] = 'Assignment settings';
+
 $string['boardbuffer'] = 'Days after resit board that grades can be released';
+
 $string['connectiondatabase'] = 'Quercus connection database';
+$string['connectiondatabaserror'] = 'There was a problem connecting to the database: {$a}';
 $string['connectionhost'] = 'Quercus connection host';
 $string['connectionpassword'] = 'Quercus connection password';
 $string['connectionsettings'] = 'Data import connection settings';
@@ -42,49 +44,58 @@ $string['connectionsettingserror'] = 'The settings for the connection are not co
 $string['courselimit'] = 'Course limit';
 $string['coursesview'] = 'Courses view name';
 $string['createmodulelimit'] = 'Number of modules to create per batch';
-$string['createnewmodules'] = 'Create new modules';
 $string['createmodulessettings'] = 'Create new modules settings';
+$string['createnewmodules'] = 'Create new modules';
 $string['cutoffinterval'] = 'Cut off date interval in weeks';
 $string['cutoffintervalsecondplus'] = 'Cut off date interval in weeks for second/third+ sittings';
 
-$string['connectiondatabaserror'] = 'There was a problem connecting to the database: {$a}';
 $string['datafile'] = 'Assignment XML data file path';
 $string['dataviews'] = 'Quercus data views';
 $string['deletecategories'] = 'Delete categories';
 $string['deletecourses'] = 'Delete courses';
 $string['deleted'] = 'Deleted {$a->shortname} - {$a->fullname}';
 $string['deleting'] = 'Deleting {$a->shortname} - {$a->fullname}';
+
 $string['emaillt'] = 'LT email address';
 $string['enrolmentview'] = 'Enrolment view name';
 $string['errordeleting'] = 'Error deleting {$a->shortname} - {$a->fullname}';
 $string['exportgrades'] = 'Export grades';
 $string['exportmodulelimit'] = 'Number of modules to export per batch';
 $string['exportsettings'] = 'Export connection details';
+
 $string['getnewcourses'] = 'Get new courses';
 $string['getnewgrades'] = 'Get new grades';
 $string['getnewmodules'] = 'Get new modules';
+$string['grademarkexemptscale'] = 'ID of grademark exempt gradescale';
+$string['grademarkscale'] = 'ID of grademark gradescale';
 $string['gradingdueinterval'] = 'Grading due date interval in weeks';
 $string['gradinghelpurl'] = 'Grading help guide URL';
-$string['grademarkscale'] = 'ID of grademark gradescale';
-$string['grademarkexemptscale'] = 'ID of grademark exempt gradescale';
+
 $string['invalidstudent'] = 'Student ID invalid of {$a->idnumber} for {$a->firstname} {$a->lastname}';
-$string['moduletemplate'] = 'Module template id';
+
 $string['modulesview'] = 'Modules view name';
-$string['notcreatederror'] = '{$a->type} not created ';
+$string['moduletemplate'] = 'Module template id';
+
 $string['nodelete'] = 'Nothing to delete';
 $string['noendpointset'] = 'No endpoint set';
 $string['nomodules'] = 'No new modules';
 $string['nopatherror'] = 'Cannot resolve category path ';
+$string['notcreatederror'] = '{$a->type} not created ';
 
-$string['ocierror'] = 'OCI is not installed';
 $string['ociconnectiontests'] = 'OCI connection tests';
+$string['ocierror'] = 'OCI is not installed';
+
+$string['pluginname'] = 'Quercus Tasks';
 
 $string['quercus_tasks:releasegrades'] = 'Release grades to Quercus';
+
 $string['releaseroles'] = 'Comma separated list of role IDs that can release grades (no spaces)';
+
 $string['srsgws'] = 'SRS-GWS URL';
 $string['staffenrolments'] = 'Staff external database enrolments';
 $string['supportsettings'] = 'Support settings';
 
 $string['testconnection'] = 'Test connection';
+
 $string['updatedates'] = 'Update assignment dates';
 $string['updatemodules'] = 'Update module dates';
